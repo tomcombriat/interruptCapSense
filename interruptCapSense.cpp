2bc426b754076792d95eb88f8d79362794436b3c
tomcombriat <tomcombriat@live.fr>
2020-09-23 18:51:49 +0200

    Starting definitions of members

--- interruptCapSense.cpp
+++ interruptCapSense.cpp
@@ -1,1 +1,17 @@
-//
+/* 
+Combriat 2020
+Manager for interrupted capacitive sensing
+*/
+
+#include "interruptCapSense.h"
+
+
+interruptCapSense::interruptCapSense(int _sendPin, const int _samples, unsigned long _timout, unsigned long _delay)
+{
+  samples = _samples;
+  sendPin = _sendPin;
+  timout = _timout;
+  delay = _delay;
+  times = new unsigned long[samples];
+  for (int i=0; i<samples;i++) times[i] = 0;
+}
--- interruptCapSense.h
+++ interruptCapSense.h
@@ -1,1 +1,37 @@
-//
+/* 
+Combriat 2020
+Manager for interrupted capacitive sensing
+*/
+
+
+#ifndef _INTCAPSENSE_
+#define _INTCAPSENSE_
+
+
+class interruptCapSense {
+  
+ public:
+  
+  interruptCapSense(int _sendPin, const int _samples = 20, unsigned long _timout = 10 , unsigned long _delay = 1);
+  void init();
+  volatile unsigned long ISR_target;
+  bool update();
+  unsigned long getCumul();
+  float getAverage();
+
+
+ private:
+  int samples;
+  unsigned long *times;
+  int runner=0;
+  unsigned long last_timing;
+  int state = 0;
+  int sendPin;
+  unsigned long timout, delay;
+  
+
+
+
+};
+
+#endif
