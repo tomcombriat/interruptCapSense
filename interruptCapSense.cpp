c1258e081659cd881b35ccb6d025247b2b64ff01
tomcombriat <tomcombriat@live.fr>
2020-09-23 20:01:34 +0200

    Drafted main function

--- interruptCapSense.cpp
+++ interruptCapSense.cpp
@@ -1,6 +1,6 @@
 /* 
-Combriat 2020
-Manager for interrupted capacitive sensing
+   Combriat 2020
+   Manager for interrupted capacitive sensing
 */
 
 #include "interruptCapSense.h"
@@ -13,5 +13,51 @@ interruptCapSense::interruptCapSense(int _sendPin, const int _samples, unsigned 
   timout = _timout;
   delay = _delay;
   times = new unsigned long[samples];
+  pinMode(sendPin, OUTPUT);
   for (int i=0; i<samples;i++) times[i] = 0;
+}
+
+interruptCapSense::~interruptCapSense()
+{
+  delete times;
+}
+
+
+bool interruptCapSense::update()
+{
+  bool ret = false;
+  switch (state)
+    {
+    case 0:  // initial
+      digitalWrite(sendPin, HIGH);
+      last_timing = micros();
+      ISR_target = 0;
+      state +=1;
+      break;
+      
+    case 1:  // waiting for the ISR to fire
+      if (ISR_target != 0)
+	{
+	  times[runner] = ISR_target - last_timing;
+	  runner += 1;
+	  if (runner == samples) runner = 0;
+	  ret = true;
+	  state += 1;
+	  digitalWrite(sendPin, LOW);
+	  last_timing = micros();
+	}
+      else if (micros() - last_timing > timout)
+	{
+	  state +=1;
+	  digitalWrite(sendPin, LOW);
+	  last_timing = micros();
+	}
+      break;
+
+    case 2:
+      if (micros() - last_timing > delay) state = 0;
+      break;
+      
+    }
+  return ret;
 }
--- interruptCapSense.h
+++ interruptCapSense.h
@@ -7,12 +7,16 @@ Manager for interrupted capacitive sensing
 #ifndef _INTCAPSENSE_
 #define _INTCAPSENSE_
 
+#include <Arduino.h>
+
+#include <inttypes.h>
 
 class interruptCapSense {
   
  public:
   
-  interruptCapSense(int _sendPin, const int _samples = 20, unsigned long _timout = 10 , unsigned long _delay = 1);
+  interruptCapSense(int _sendPin, const int _samples = 20, unsigned long _timout = 1000 , unsigned long _delay = 1000);
+  ~interruptCapSense();
   void init();
   volatile unsigned long ISR_target;
   bool update();
